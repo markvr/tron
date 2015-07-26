04de33d94067a2acb0e641bd4756aa4b75061c0a
mark <markvanrossum@yahoo.co.uk>
2015-07-26 16:33:14 +0100

    Improved sparkles changing hue, so the hue can change more slowly by bring in n sparkles of the same hue each time

--- mode_sparkles.cpp
+++ mode_sparkles.cpp
@@ -14,8 +14,17 @@ void mode_sparkles(bool firstRun, int mode) {
 	static long lastUpdate = 0;
 	static int numLit = 0;
 	static unsigned int hue = 0;
+	const int MAX_SPEED = 10;
+	const int MAX_CHANGE_SPEED = 10;
+	// Counts how many sparkles for each new hue have been added
+	// Used to add n sparkles for each hue when changing to slow the 
+	// speed down
+	static int hueSparklesAdded = 0;
+	// Tracks the current hue colour we are adding
+	static int currentHue = 0;
+
 	int hueChangeSpeed = 1;
-	int newNumLit = getModeSetting(MODE_SPARKLES + mode, 0) * 10;	
+	int desiredNumLit = getModeSetting(MODE_SPARKLES + mode, 0) * 10;	
 	int speed = getModeSetting(MODE_SPARKLES + mode, 1);
 	int sat = 255;
 	if (mode == 0) {
@@ -39,7 +48,7 @@ void mode_sparkles(bool firstRun, int mode) {
 	
 	if (firstRun) {
 		// Reset everything
-		numLit = newNumLit;
+
 		for (int i = 0; i < NUM_LEDS; i++) {
 			unlit[i] = i;
 			lit[i] = 0;
@@ -51,7 +60,7 @@ void mode_sparkles(bool firstRun, int mode) {
 		// Randomise the arrays at the start.  The basic idea is to choose an "unlit" LED at random, copy it's
 		// number to the "lit" array, and then remove it from the "unlit" array.  We "remove" it, by shifting
 		// all the values above it down by one bin, and putting 0 at the top.
-		for (int i = 0; i < numLit; i++) {
+		for (int i = 0; i < desiredNumLit; i++) {
 			int bin = random16(NUM_LEDS - i);	// Choose an unlit LED from the remaining ones (NUM_LEDS - i)
 			lit[i] = unlit[bin];				// Move it to the lit array
 			phase[unlit[bin]] = random8(64) * 512; // Give it a random start value that is a multiple of 512
@@ -69,47 +78,49 @@ void mode_sparkles(bool firstRun, int mode) {
 			}
 			unlit[NUM_LEDS - 1] = 0;
 		}
+		numLit = desiredNumLit;
 	}
 
 	// "Number Lit" dial
-	if (numLit != newNumLit) {
-		int difference = newNumLit - numLit;
-		if (difference > 0) {	// Going Up
-			for (int i = 0; i < difference; i++) {
-				if (numLit < NUM_LEDS) {	
-					int bin = random16(NUM_LEDS - numLit);	// Choose a random unlit led
-					lit[numLit] = unlit[bin];		// Move it to the lit array
-					phase[unlit[bin]] =  random8(64) * 512; //randomize it's value
-					switch (mode) {
-					case 0:
-						hues[unlit[bin]] = getModeSetting(MODE_SPARKLES, 2) * 10;
-						break;
-					case 1:
-						hues[unlit[bin]] = hue;
-						break;
-					case 2:
-						hues[unlit[bin]] = random8();
-						break;
-					}
+	if (desiredNumLit > numLit) {
+		// Going up
+		for (int i = 0; i < desiredNumLit - numLit; i++) {
+			if (numLit < NUM_LEDS) {
+				int bin = random16(NUM_LEDS - numLit);	// Choose a random unlit led
+				lit[numLit] = unlit[bin];		// Move it to the lit array
+				phase[unlit[bin]] = random8(64) * 512; //randomize it's value
+				switch (mode) {
+				case 0:
+					hues[unlit[bin]] = getModeSetting(MODE_SPARKLES, 2) * 10;
+					break;
+				case 1:
+					hues[unlit[bin]] = hue;
+					break;
+				case 2:
+					hues[unlit[bin]] = random8();
+					break;
+				}
 
-					// and remove it from the unlit array
-					for (int j = bin; j < NUM_LEDS - 1; j++) {
-						unlit[j] = unlit[j+1];
-					}
-					numLit++;
+				// and remove it from the unlit array
+				for (int j = bin; j < NUM_LEDS - 1; j++) {
+					unlit[j] = unlit[j + 1];
 				}
+				numLit++;
 			}
-		} else {	// going down...
-			for (int i = 0; i < difference; i++) {
-				if (numLit > 0) {  // going down
-					unlit[NUM_LEDS - numLit] = lit[numLit]; // move the top most "lit" led to the unlit array
-					phase[lit[numLit]] = 0;	// and turn it out etc.
-					lit[numLit] = 0;
-					numLit--;
-				}
+		}
+	} else if (desiredNumLit < numLit) {	// going down...
+		int difference = numLit - desiredNumLit;
+		p("going down, different %u\n", difference);
+		for (int i = 0; i < difference; i++) {
+			if (numLit > 0) {  // going down
+				unlit[NUM_LEDS - numLit] = lit[numLit]; // move the top most "lit" led to the unlit array
+				phase[lit[numLit]] = 0;	// and turn it out etc.
+				lit[numLit] = 0;
+				numLit--;
 			}
 		}
 	}
+
 	
 	
 	if  (millis() - lastUpdate > 200 / speed ) {
@@ -117,7 +128,7 @@ void mode_sparkles(bool firstRun, int mode) {
 		lastUpdate = millis();
 		for (int i = 0; i < numLit; i++) {
 			leds[lit[i]] = CHSV(hues[lit[i]], sat, sin16(phase[lit[i]]) >> 8);
-			phase[lit[i]] += 2048;
+			phase[lit[i]] += 512;
 			if (phase[lit[i]] > 32767) { // One of them has gone out,
 				// We randomly choose a new LED from the unlit array, and swap it with the lit one that has gone out.
 				// Over time the arrays will both become randomly ordered, but this doesn't matter.
@@ -126,14 +137,30 @@ void mode_sparkles(bool firstRun, int mode) {
 				lit[i] = unlit[bin]; // And swap them over
 				unlit[bin] = temp;
 				phase[lit[i]] = 0;	// Set the new value to 0;
+
+				int changeSpeed = getModeSetting(MODE_SPARKLES + 1, 2);
 				switch (mode) {
 				case 0:
 					hues[lit[i]] = getModeSetting(MODE_SPARKLES, 2) * 10;
 					break;
 				case 1:
-					hue = hue + getModeSetting(MODE_SPARKLES + 1, 2);
-					if (hue > 2048) hue = 0;
-					hues[lit[i]] = hue >> 3;
+					/*
+					We want to add sparkles so that the colour changes at a constant
+					rate regardless of the speed of the leds twinkling.
+					
+					*/
+					if (hueSparklesAdded <= (MAX_CHANGE_SPEED - changeSpeed)) {
+						// We need to add sparkles of the existing hue
+						p("adding sparkle, hSA %u\n", hueSparklesAdded);
+						hueSparklesAdded = hueSparklesAdded + (MAX_SPEED - speed) + 1;
+					} else {
+						p("adding hue, currentHue %u\n", currentHue);
+						hueSparklesAdded = 1;
+						currentHue++;
+						if (currentHue > 256) currentHue = 0;
+					}
+					hue = currentHue;
+					hues[lit[i]] = hue;
 					break;
 				case 2:
 					hues[lit[i]] = random8();
